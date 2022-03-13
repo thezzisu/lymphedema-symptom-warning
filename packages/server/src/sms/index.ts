1dfccab6aec4b5d618ab49c8681d0932be4eeee3
thezzisu <thezzisu@gmail.com>
2022-03-13 12:05:27 +0800

    feat: add tencent cloud sdk

--- packages/server/src/sms/index.ts
+++ packages/server/src/sms/index.ts
@@ -1,5 +1,19 @@
 import NodeCache = require('node-cache')
 import { logger } from '../util'
+import * as tencentcloud from 'tencentcloud-sdk-nodejs-sms'
+import { readFileSync } from 'fs'
+
+const config = JSON.parse(readFileSync('sms.json').toString())
+
+const Client = tencentcloud.sms.v20210111.Client
+const client = new Client({
+  credential: {
+    secretId: config.credential.id,
+    secretKey: config.credential.key
+  },
+  region: 'ap-guangzhou',
+  profile: {}
+})
 
 const TTL = 5 * 60 // 5 minutes
 
@@ -13,6 +27,13 @@ export async function sendCode(tel: string) {
   const code = generateCode()
   cache.set(tel, code)
   logger.info(`Send code ${code} to ${tel}`)
+  await client.SendSms({
+    SmsSdkAppId: config.appId,
+    PhoneNumberSet: ['+86' + tel],
+    SignName: config.signName,
+    TemplateId: config.templateId,
+    TemplateParamSet: [code]
+  })
 }
 
 export async function verifyCode(tel: string, code: string) {
